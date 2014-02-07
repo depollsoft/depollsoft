4a85d05900569e14d090d359d28894d28c3bfbce
depoll <david@depoll.com>
2014-02-07 12:22:43 -0800

    First steps.

---
+++ Android/PitchPerfect/src/depollsoft/pitchperfect/PitchPipeAppWidget.java
@@ -0,0 +1,11 @@
+package depollsoft.pitchperfect;
+
+import android.appwidget.AppWidgetProvider;
+
+public class PitchPipeAppWidget extends AppWidgetProvider {
+
+  public PitchPipeAppWidget() {
+    // TODO Auto-generated constructor stub
+  }
+
+}
