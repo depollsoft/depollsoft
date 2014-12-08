4179e26d5947560622f3fb6b415d6956b746726d
David Poll <david@depoll.com>
2014-12-07 17:20:09 -0800

    Remove admob.

--- DotNet/DePhoneTunes/DePhoneTunes/AdControl.xaml.cs
+++ DotNet/DePhoneTunes/DePhoneTunes/AdControl.xaml.cs
@@ -10,48 +10,7 @@ namespace DePhoneTunes {
     private Microsoft.Advertising.Mobile.UI.AdControl pubcenterControl;
     public AdControl() {
       InitializeComponent();
-      try {
-        var request = WebRequest.Create("http://apps.davidpoll.com/pitchperfect/adType.php");
-        request.BeginGetResponse(res => {
-              try {
-                var response = request.EndGetResponse(res);
-                StreamReader sr = new StreamReader(response.GetResponseStream());
-                string adType = sr.ReadToEnd().Trim();
-                if (adType.Equals("admob", StringComparison.OrdinalIgnoreCase)) {
-                  Dispatcher.BeginInvoke(LoadAdmob);
-                } else {
-                  Dispatcher.BeginInvoke(LoadPubcenter);
-                }
-              } catch {
-                Dispatcher.BeginInvoke(LoadPubcenter);
-              }
-            }, null);
-      } catch {
-        LoadPubcenter();
-      }
-    }
-
-    private void LoadAdmob() {
-      admobControl = new BannerAd();
-#if TESTADS
-#else
-#if ADS
-            admobControl.AdUnitID = "ca-app-pub-2903856737482177/7572933041";
-            admobControl.KeywordList = "music|notes|barbershop|sing|pitch pipe|pitch|guitar".Split('|');
-#else
-      this.Visibility = System.Windows.Visibility.Collapsed;
-#endif
-#endif
-      //#if TESTADS
-      //            Microsoft.Advertising.Mobile.UI.AdControl.TestMode = true;
-      //            adControl.ApplicationId = "test_client";
-      //            adControl.AdUnitId = "Image480_80";
-      //#else
-      //            Microsoft.Advertising.Mobile.UI.AdControl.TestMode = false;
-      //#endif
-#if ADS
-            adContainer.Content = admobControl;
-#endif
+      LoadPubcenter();
     }
 
     private void LoadPubcenter() {
@@ -62,7 +21,6 @@ namespace DePhoneTunes {
             pubcenterControl.AdUnitId = "10013845";
             pubcenterControl.ApplicationId = "54a78009-dbac-40be-84c5-d84b0008fffe";
             pubcenterControl.AdSelectionKeywords = "music|notes|barbershop|sing|pitch pipe|pitch|guitar";
-            pubcenterControl.AdControlError += new EventHandler<Microsoft.Advertising.Mobile.UI.ErrorEventArgs>(adControl_AdControlError);
 #else
       this.Visibility = System.Windows.Visibility.Collapsed;
 #endif
@@ -77,10 +35,6 @@ namespace DePhoneTunes {
 #if ADS
             adContainer.Content = pubcenterControl;
 #endif
-    }
-
-    void adControl_AdControlError(object sender, Microsoft.Advertising.Mobile.UI.ErrorEventArgs e) {
-      Dispatcher.BeginInvoke(LoadAdmob);
     }
   }
 }
