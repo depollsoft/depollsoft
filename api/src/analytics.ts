7720c22e198cc79676dbaa82df3b7dd300dfbd15
David Poll <david@depoll.com>
2021-06-16 09:06:03 -0700

    Adds analytics API

--- api/src/analytics.ts
+++ api/src/analytics.ts
@@ -1,14 +1,42 @@
 import express from 'express';
 import { OAuth2Client } from 'google-auth-library';
 import { PubSub } from '@google-cloud/pubsub';
+import { BigQuery } from '@google-cloud/bigquery';
+import { lookup } from 'geoip-lite';
+import { PartialFailureError } from '@google-cloud/common/build/src/util';
 
 const authClient = new OAuth2Client();
 const pubSub = new PubSub();
+const bigQuery = new BigQuery();
+const analyticsTable = bigQuery.dataset('app_analytics').table('events');
 const { PUBSUB_VERIFICATION_TOKEN } = process.env;
 
 const router = express.Router()
     .use(express.json())
-    .post('/', (req, res) => {
+    .post('/', async (req, res) => {
+        const ip = req.headers['x-forwarded-for'] as string ?? req.socket.remoteAddress ?? '127.0.0.1';
+        const location = lookup(ip);
+        await pubSub.topic('analytics').publishJSON({
+            ...req.body,
+            location: location ? {
+                range: {
+                    low: location.range[0],
+                    high: location.range[1]
+                },
+                country: location.country,
+                region: location.region,
+                eu: location.eu == '1',
+                timezone: location.timezone,
+                city: location.city,
+                coordinates: {
+                    latitude: location.ll[0],
+                    longitude: location.ll[1],
+                    radius: location.area
+                },
+                metro: location.metro
+            } : null,
+        });
+        res.status(201).send();
     })
     .post('/pubsub', async (req, res) => {
         // Verify that the request originates from the application.
@@ -53,6 +81,25 @@ const router = express.Router()
         const message = Buffer.from(req.body.message.data, 'base64').toString(
             'utf-8'
         );
+
+        const messageData = JSON.parse(message);
+
+        try {
+            await analyticsTable.insert([
+                {
+                    ...messageData,
+                    publish_timestamp: req.body.message.publishTime,
+                }
+            ], {
+                ignoreUnknownValues: true,
+            });
+        } catch (e) {
+            if (e instanceof PartialFailureError) {
+                console.error(e);
+            } else {
+                throw e;
+            }
+        }
 
         res.status(200).send();
     });
