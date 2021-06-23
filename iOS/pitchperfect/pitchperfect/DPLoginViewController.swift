7b49e0ac9388ef008fda6d86f18c0ed32d75334d
David Poll <david@depoll.com>
2021-06-23 11:29:19 -0700

    Get rid of unnecessary login screen

--- iOS/pitchperfect/pitchperfect/DPLoginViewController.swift
+++ iOS/pitchperfect/pitchperfect/DPLoginViewController.swift
@@ -15,27 +15,26 @@ import FirebaseEmailAuthUI
 import FirebaseFacebookAuthUI
 
 extension DPLoginViewController: FUIAuthDelegate {
+    @objc public func logIn(_ viewController:UIViewController) {
+        let authUI = FUIAuth.defaultAuthUI()!
+        let providers: [FUIAuthProvider] = [
+            FUIEmailAuth(authAuthUI: authUI,
+                         signInMethod: EmailPasswordAuthSignInMethod,
+                         forceSameDevice: false,
+                         allowNewEmailAccounts: true,
+                         requireDisplayName: false,
+                         actionCodeSetting: ActionCodeSettings()),
+            FUIGoogleAuth(authUI: authUI),
+            FUIFacebookAuth(authUI: authUI),
+            FUIOAuth.appleAuthProvider()
+        ]
+        authUI.providers = providers
+        authUI.delegate = self
+        viewController.present(authUI.authViewController(), animated: true)
+    }
+    
     @objc func logInClick() {
-        if Auth.auth().currentUser == nil {
-            let authUI = FUIAuth.defaultAuthUI()!
-            let providers: [FUIAuthProvider] = [
-                FUIEmailAuth(authAuthUI: authUI,
-                             signInMethod: EmailPasswordAuthSignInMethod,
-                             forceSameDevice: false,
-                             allowNewEmailAccounts: true,
-                             requireDisplayName: false,
-                             actionCodeSetting: ActionCodeSettings()),
-                FUIGoogleAuth(authUI: authUI),
-                FUIFacebookAuth(authUI: authUI),
-                FUIOAuth.appleAuthProvider()
-            ]
-            authUI.providers = providers
-            authUI.delegate = self
-            
-            self.present(authUI.authViewController(), animated: true)
-        } else {
-            try! Auth.auth().signOut()
-        }
+        logIn(self)
     }
     
     public func authUI(_ authUI: FUIAuth, didSignInWith authDataResult: AuthDataResult?, error: Error?) {
