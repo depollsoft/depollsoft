0919ebf64a56b46087dce32712374d6b08674605
David Poll <david@depoll.com>
2022-09-21 19:00:56 -0700

    Fix license and widget

--- Android/PitchPerfect/src/depollsoft/pitchperfect/PitchPipeAppWidget.java
+++ Android/PitchPerfect/src/depollsoft/pitchperfect/PitchPipeAppWidget.java
@@ -7,7 +7,9 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
@@ -32,6 +34,11 @@ public class PitchPipeAppWidget extends AppWidgetProvider {
   private PitchPipeModel model = new PitchPipeModel();
 
   public PitchPipeAppWidget() {
+  }
+
+  @Override
+  public void onReceive(Context context, Intent intent) {
+    super.onReceive(context, intent);
   }
 
   public static void updateWidgets() {
@@ -77,7 +84,8 @@ public class PitchPipeAppWidget extends AppWidgetProvider {
     RemoteViews rv = new RemoteViews(context.getPackageName(), R.layout.pitchpipewidgetview);
 
     for (int i = 0; i < ids.length; i++) {
-      rv.setImageViewUri(ids[i], buildUpdate(context, model.getNotes().get(i)));
+      rv.setImageViewBitmap(ids[i], buildUpdateBitmap(context, model.getNotes().get(i)));
+      //rv.setImageViewUri(ids[i], buildUpdate(context, model.getNotes().get(i)));
       rv.setOnClickPendingIntent(ids[i], getIntentForNote(model.getNotes().get(i), i));
       int pressed = R.drawable.button_background_pressed;
       int normal = R.drawable.button_background_normal;
@@ -87,7 +95,7 @@ public class PitchPipeAppWidget extends AppWidgetProvider {
       rv.setInt(ids[i], "setBackgroundResource", background);
     }
 
-    PendingIntent launchPitchPipe = PendingIntent.getActivity(context, 0, new Intent(context, PitchPerfectActivity.class), 0);
+    PendingIntent launchPitchPipe = PendingIntent.getActivity(context, 0, new Intent(context, PitchPerfectActivity.class), PendingIntent.FLAG_IMMUTABLE | PendingIntent.FLAG_UPDATE_CURRENT);
     rv.setOnClickPendingIntent(R.id.launchIcon, launchPitchPipe);
 
     appWidgetManager.updateAppWidget(new ComponentName(context, PitchPipeAppWidget.class), rv);
@@ -107,10 +115,25 @@ public class PitchPipeAppWidget extends AppWidgetProvider {
     }
     i.putExtra("octave", n.getOctave());
     //i.putExtra("play", !n.getIsPlaying());
-    return PendingIntent.getService(PitchPerfectApplication.getAppContext(), index, i, PendingIntent.FLAG_UPDATE_CURRENT);
+    return PendingIntent.getService(PitchPerfectApplication.getAppContext(), index, i, PendingIntent.FLAG_IMMUTABLE | PendingIntent.FLAG_CANCEL_CURRENT);
   }
 
   private static Map<Note, Uri> builtImages = new HashMap<>();
+
+  public Bitmap buildUpdateBitmap(Context c, Note n) {
+    CharSequence string = (CharSequence) new PitchPipeNoteTextConverter()
+            .convertToTarget(n, CharSequence.class);
+    TextView view = new TextView(PitchPerfectApplication.getAppContext());
+    view.setText(string);
+    view.setTextSize(TypedValue.COMPLEX_UNIT_SP, 30);
+    view.setTextColor(Color.WHITE);
+    view.measure(ActionBar.LayoutParams.WRAP_CONTENT, ActionBar.LayoutParams.WRAP_CONTENT);
+    view.layout(0, 0, view.getMeasuredWidth(), view.getMeasuredHeight());
+    view.buildDrawingCache(true);
+    Bitmap bmp = view.getDrawingCache(true).copy(Bitmap.Config.ARGB_4444, false);
+    view.destroyDrawingCache();
+    return bmp;
+  }
 
   public Uri buildUpdate(Context c, Note n) {
     if (builtImages.containsKey(n)) {
@@ -150,9 +173,9 @@ public class PitchPipeAppWidget extends AppWidgetProvider {
         }
       }
     }
-    Uri result = FileProvider.getUriForFile(c, c.getPackageName(), dest);
-    for (ApplicationInfo app : c.getPackageManager().getInstalledApplications(0)) {
-      c.grantUriPermission(app.packageName, result, Intent.FLAG_GRANT_READ_URI_PERMISSION);
+    Uri result = FileProvider.getUriForFile(c, c.getPackageName() + ".fileprovider", dest);
+    for (PackageInfo pkg : c.getPackageManager().getInstalledPackages(0)) {
+      c.grantUriPermission(pkg.packageName, result, Intent.FLAG_GRANT_READ_URI_PERMISSION);
     }
     builtImages.put(n, result);
     return result;
