46b14ccc5b2c1bba3e100aaa0ec02f47b3bd6dda
David Poll <david@depoll.com>
2026-02-02 05:37:22 -0800

    Fix flaky test: ensure view controller hierarchy is attached before presenting

--- iOS/tagmaster/tagmasterTests/DPHomeViewControllerExtensionTests.swift
+++ iOS/tagmaster/tagmasterTests/DPHomeViewControllerExtensionTests.swift
@@ -27,6 +27,9 @@ class DPHomeViewControllerExtensionTests: XCTestCase {
         // Load the view hierarchy
         _ = homeViewController.view
         homeViewController.view.layoutIfNeeded()
+        
+        // Give the run loop time to fully attach the view controller hierarchy
+        RunLoop.current.run(until: Date(timeIntervalSinceNow: 0.1))
     }
     
     override func tearDown() {
