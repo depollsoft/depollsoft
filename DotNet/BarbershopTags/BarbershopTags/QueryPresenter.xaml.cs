128cf0ff06c04385aea30fbb44efcae2f6694fbe
David Poll <depoll@mac-mini-pro.home>
2026-08-30 07:49:55 -0700

    Remove legacy Windows Phone code

--- DotNet/BarbershopTags/BarbershopTags/QueryPresenter.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Windows.Controls;
-
-namespace BarbershopTags {
-  public partial class QueryPresenter : UserControl {
-    public QueryPresenter() {
-      InitializeComponent();
-    }
-  }
-}
